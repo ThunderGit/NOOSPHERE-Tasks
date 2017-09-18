790edf4c78c7a80234e32bdc059e65b2414dbe34
Andrew <aivlev8@gmail.com>
2017-09-19 00:41:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var http = require('http');
-var fs = require('fs');
+const http=require("http");
+const fs=require("fs");
 var path = require('path');
-var fileName="E:/Andrew/NODEHTTP/Results1.json";
+var fileName="./Results1.json";
+var fileNameRec="./ResultsREC.txt";
 
+const server=http.createServer();
 
 const types={
 	'.js':{contentType:'text/javascript'},
@@ -12,129 +14,173 @@ const types={
 	'.png':{contentType:'image/png'},
 	default:{contentType:'text/html'}
 };
-
-http.createServer(function (request, response) {
+server.on('request',(request,response)=> {
 
 const {method,url,headers} = request;
 
 if(method==='POST'){
-    let postData="";
-    request.on('data',data=>{
+	let postData="";
+	request.on('data',data=>{
         postData+=data;
-    });
-    request.on('end',()=>{
-        processPost(request,response,postData);
-    });
+	});
+	request.on('end',()=>{
+		processPost(request,response,postData);
+	});
 }else{
-    processGet(request,response);
+	processGet(request,response);
 }
 
 function processPost(request,response,postData){
-    let data={};
-    try{ 
+	let data={};
+	try{ 
       data = JSON.parse(postData);
       response.statusCode=201;
      
-     
-     
-      //var obj= [{table: []}];
-      
-       //obj.table.push({PlayerName: data.name, Points:data.pts}); 
-       var jayson = JSON.stringify({PlayerName: data.name, Points:data.pts}); 
-        
+      fs.readFile("./Results1.json", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
 
-    fs.appendFile(fileName, jayson+"\n",'utf8', function readFileCallback(err, data){
+    
+    let obj = JSON.parse(result); // То что нужно
+    obj.push({PlayerName: data.name, Points:data.pts}); 
+       var jayson = JSON.stringify(obj); 
+       
+    fs.writeFile(fileName, jayson+"\n",'utf8', function readFileCallback(err, data){
          if (err){
             console.log(err);
           } else {
         console.log("File Saved!");
       }
     });
+    
+  });
+      
+       
      } catch(e){
         response.statusCode=400;
       }
-    console.log("\nPOST = ", data.name,data.pts);
-    
-    response.end();
+	console.log("\nPOST = ", data.name,data.pts);
+	
+	response.end();
 }
 
-function getTop10(done) {
+function getTop10() {
 
-  fs.readFile(fileName, (err, result) => {
+  fs.readFile("./Results1.json", (err, result) => {
     if (err) {
-      return done(err);
+      console.log(err);
     }
-    let data = escapeSpecialChars(JSON.stringify(result));
-    data=JSON.parse(data);
-    //data=sortAndProcess(data,"Points");
-    console.log("Success");
-    //data = sortAndProcess(data,"Points"); // sort, find, etc...
-    done(null,  data);
+
+    //result=sortAndProcess(result,"Points");
+    let data = JSON.parse(result); // То что нужно
+    //data=JSON.stringify(data);
+    //data=JSON.parse(data);
+    data = sortAndProcess(data); // sort, find, etc...
+    //console.log(data);
+    
   });
 
 }
 
-function sortAndProcess(data,key){
-  return data.sort(function(a, b) {
-        var x = a[key]; var y = b[key];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
+function sortAndProcess(jsObj){
+  	var sortPoints = [];
+  	var sortNames=[];
+  	for(var i=0;i<jsObj.length ;i++)
+  	{
+		sortPoints.push(parseInt(jsObj[i].Points));
+		sortNames.push(jsObj[i].PlayerName);
+	}
+	for(var i=0;i<sortPoints.length;i++)
+	{
+		for(var j=0;j<sortPoints.length-1;j++)
+		{
+          if(sortPoints[j]<sortPoints[j+1]){
+
+          	var temp=sortPoints[j];
+            sortPoints[j]=sortPoints[j+1];
+            sortPoints[j+1]=temp;
+
+            temp=sortNames[j];
+            sortNames[j]=sortNames[j+1];
+            sortNames[j+1]=temp;
+          }
+		}
+	}
+	var recList;;
+	var size=10;
+	if(size>sortPoints.length) {
+		size=sortPoints.length;
+	}
+	for(var i=0;i<10 ;i++)
+  	{
+	       recList+=sortNames[i]+": "+sortPoints[i]+"<br>\n";
+	}
+	recList=recList.substring(9);
+	console.log(recList);
+	 fs.writeFile(fileNameRec, recList,'utf8', function readFileCallback(err, data){
+         		if (err){
+            		console.log(err);
+          		} else {
+        		console.log("File Saved!");
+      		}
+    	});	
+
 }
 
-function escapeSpecialChars(jsonString) {
+// function escapeSpecialChars(jsonString) {
 
-            return jsonString.replace(/\n/g, "\\n")
-                .replace(/\r/g, "\\r")
-                .replace(/\t/g, "\\t")
-                .replace(/\f/g, "\\f");
+//             return jsonString.replace(/\n/g, "\\n")
+//                 .replace(/\r/g, "\\r")
+//                 .replace(/\t/g, "\\t")
+//                 .replace(/\f/g, "\\f");
 
-}
+// }
+
 function processGet(request,response){
-//console.log(request.url+"\n");
+//console.log('Request= ${method},${url}');
+// console.log(headers);
+	let url = request.url;
+	let fileExt = path.extname(url);
 
-var filePath = '.' + request.url;
+	  if(url=="/Results1.json"){
+		getTop10((err, top10data) => {
+			  if (err) {
+				  console.log('Error happened!');
+			  } else {
 
-if (filePath == './'||filePath == './public/RemoveBlocks/index.html?')
-    filePath = './public/RemoveBlocks/index.html';
+				  console.log('Top 10 results:', top10data);
+			  
+			      response.write(top10Data.toString());
+			      response.end();
+			  }
+	 	});
+	  }
+	else{
+	    let responseParams=types[fileExt]||types.default;
 
-if(filePath=='./public/RemoveBlocks/About.html?')
-    filePath = './public/RemoveBlocks/About.html';
 
-let fileExt = path.extname(request.url);
+	    response.setHeader("Content-Type",responseParams.contentType);
 
-let responseParams=types[fileExt]||types.default;
 
-response.setHeader("Content-Type",responseParams.contentType);
+	    let filepath="."+url;
+		if(filepath==="./"){
 
-fs.readFile(filePath, function(error, content) {
-    if (error) {
-        if(error.code == 'ENOENT'){
-           response.writeHead(400);
-            response.end('<h1>ERROR 404</h1>\n <p>Page Not Found</p>\n');
-            response.end();
-        }
-        else {
-            response.writeHead(500);
-            response.end('Sorry, check with the site admin for error\n');
-            response.end(); 
-        }
-    }
-    else {
-        response.writeHead(200, { 'Content-Type': responseParams.contentType });
-        response.end(content, 'utf-8');
-    }
-});
+			filepath="./public/RemoveBlocks/index.html";
+		}
 
-getTop10((err, top10data) => {
-  if (err) {
-      console.log('Error happened!');
-  } else {
-      console.log('Top 10 results:', top10data);
-      //var rec=document.getElementById("record");
-      //rec.innerHTML=top10data;
-  }
-});
+	    console.log(filepath);
+
+		let readStream=fs.createReadStream(filepath);
+		readStream.pipe(response);
+        readStream.on('error', function() {
+            response.statusCode = 404;
+			response.end();
+        });
+	}
 
 }
+//response.end();
+});
 
-}).listen(8080);
+server.listen(8080);
