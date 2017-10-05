569a6938f55d56742c5c2a468c03259eda6cac8b
Andrew <aivlev8@gmail.com>
2017-10-05 14:37:38 +0300

    Update auth_controller.js

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -6,41 +6,41 @@ var fileName="../public/Users.json";
 module.exports={
 
 	index:function(req,res,next){
-        res.render("../views/auth.ejs",{req:req}); 
+        res.render("../views/auth.ejs"); 
 	},
 
-	login: function(req,res,next){
-		
-		fs.readFile(fileName, (err, result) => {
-              if (err) { console.log(err); }
+	auth: function(req,res,next){
+		req.session.isLoggedIn=false;
+		// fs.readFile(fileName, (err, result) => {
+  //             if (err) { console.log(err); }
 
-              let obj = JSON.parse(result); // То что нужно
+  //             let obj = JSON.parse(result); // То что нужно
 
     
-              obj.push({Name: req.body.name, Login:req.body.login,Password:req.body.pass}); 
-              var jayson = JSON.stringify(obj); 
+  //             obj.push({Name: req.body.name, Login:req.body.login,Password:req.body.pass}); 
+  //             var jayson = JSON.stringify(obj); 
        
 
-              fs.writeFile(fileName, jayson+"\n",'utf8', function readFileCallback(err, data){
-                    if (err){
-                      console.log(err);
-                      } 
-                    else 
-                    {
-                      console.log("User Saved!");
-                    }
-              });
+  //             fs.writeFile(fileName, jayson+"\n",'utf8', function readFileCallback(err, data){
+  //                   if (err){
+  //                     console.log(err);
+  //                     } 
+  //                   else 
+  //                   {
+  //                     console.log("User Saved!");
+  //                   }
+  //             });
     
-            });
-		// req.session.isLoggedIn=true;
-		// console.log("req.session.isLoggedIn: "+req.session.isLoggedIn);
-		// res.redirect(301,'/');
+  //           });
+		req.session.isLoggedIn=true;
+		console.log("req.session.isLoggedIn: "+req.session.isLoggedIn);
+		res.redirect(301,'/');
 	},
 
 	logout:function(req,res,next){
 		req.session.destroy(function(err){
 
-			res.redirect(301,'/auth');
+			res.redirect(301,'/login');
 		});
 	}
 
