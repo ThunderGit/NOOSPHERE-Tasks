44e141e16811982867c681d2ada37fdbd7e3de58
Andrew <aivlev8@gmail.com>
2017-10-05 14:36:38 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@ var router = express.Router();
 
 var indexController=require('../controllers/index_controller.js');
 
-// function checkAuth(req,res,next){
-// 	if(req.session.isLoggedIn){
-// 		return next();
-// 	}else{
-// 		res.status(401);
-// 		res.redirect('auth');
-// 	}
-// }
+function checkAuth(req,res,next){
+	if(req.session.isLoggedIn){
+		return next();
+	}else{
+		res.status(401);
+		res.redirect('login');
+	}
+}
 
-router.get('/', indexController.index);
+router.get('/', checkAuth,indexController.index);
 router.post('/', indexController.save);
-router.post('/auth', indexController.save);
+//router.post('/auth', indexController.save);
 module.exports = router;
